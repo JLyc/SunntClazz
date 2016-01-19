cc4f11c1637c73cd4e9911509639c527781db80a
JLyc <andrej.socha@gmail.com>
2016-01-19 05:11:08 +0100

    Add listener a mock (reply to)

--- RvSender/src/gui/MainFrame.java
+++ RvSender/src/gui/MainFrame.java
@@ -1,5 +1,7 @@
 package gui;
 
+import com.tibco.tibrv.TibrvException;
+import rv_wrappers.RVMessageInterfaceWraper;
 import rv_wrappers.RvCommunication;
 
 import javax.swing.*;
@@ -12,44 +14,61 @@ import java.awt.event.ActionListener;
  * Created by JLyc.Development@gmail.com on 1/19/2016.
  */
 public class MainFrame extends JFrame implements ActionListener {
+    private static final String NEW_MSG = "\n******* New MSG ********\n";
 
-    private static  JTextField rvNetwork;
-    private static  JTextField rvDeamon;
-    private static  JTextField rvService;
-    private static  JTextField rvSubject;
-    private static  JTextArea rvMsg;
-    private static  JTextField isConnected;
-    private JButton connect;
+    private static JTextField rvNetwork;
+    private static JTextField rvDeamon;
+    private static JTextField rvService;
+    private static JTextField rvSubject;
+    private static JTextArea rvMsg;
+    private static JTextField isConnected;
+    private static JCheckBox replyCheckBox = new JCheckBox("Auto reply with msg", false);
+    private static JButton connect  = new JButton("Connect");
     private static JTextArea status = new JTextArea("Connect to rv deamon");
-    DefaultCaret caret = (DefaultCaret)status.getCaret();
+    private static JTextArea replyMsg = new JTextArea("<reply_msg>\n\n" +
+            "</reply_msg>");
+    DefaultCaret caret = (DefaultCaret) status.getCaret();
 
     private JButton sendMsg;
+    private static JTextArea receivedMsg = new JTextArea(NEW_MSG);
+    private static JButton listener = new JButton("Start Listener");
 
-    public static  JTextField getRvNetwork() {
+    public static JButton getListener() {
+        return listener;
+    }
+
+    public static JButton getConnect(){
+        return connect;
+    }
+
+
+    private JTextArea listenerText;
+
+    public static JTextField getRvNetwork() {
         return rvNetwork;
     }
 
-    public static  JTextField getRvDeamon() {
+    public static JTextField getRvDeamon() {
         return rvDeamon;
     }
 
-    public static  JTextField getRvService() {
+    public static JTextField getRvService() {
         return rvService;
     }
 
-    public static  JTextField getRvSubject() {
+    public static JTextField getRvSubject() {
         return rvSubject;
     }
 
-    public static  JTextArea getRvMsg() {
+    public static JTextArea getRvMsg() {
         return rvMsg;
     }
 
-    public static  JTextArea getStatus() {
+    public static JTextArea getStatus() {
         return status;
     }
 
-    public MainFrame(){
+    public MainFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setName("RvTool");
         this.setMinimumSize(new Dimension(500, 400));
@@ -63,7 +82,7 @@ public class MainFrame extends JFrame implements ActionListener {
 
         caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
         JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(8,2));
+        mainPanel.setLayout(new GridLayout(11, 2));
 
         JLabel rvDeamonText = new JLabel("RvDeamon");
         rvDeamon = new JTextField("7500");
@@ -81,12 +100,11 @@ public class MainFrame extends JFrame implements ActionListener {
         jScrollPane.getViewport().add(rvMsg);
         JScrollPane jScrollPaneStatus = new JScrollPane();
         jScrollPaneStatus.getViewport().add(status);
-        DefaultCaret caret = (DefaultCaret)status.getCaret();
+        DefaultCaret caret = (DefaultCaret) status.getCaret();
         caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
 
         isConnected = new JTextField("Not Connected");
         isConnected.setEditable(false);
-        connect = new JButton("Connect");
         connect.addActionListener(this);
 
 
@@ -94,6 +112,18 @@ public class MainFrame extends JFrame implements ActionListener {
         sendMsg = new JButton("Send");
         sendMsg.setEnabled(false);
         sendMsg.addActionListener(this);
+
+        listenerText = new JTextArea("Not Running");
+        listenerText.setEditable(false);
+        listener.setEnabled(false);
+        listener.addActionListener(this);
+        JLabel received = new JLabel("Received msg");
+        JScrollPane jScrollPanelReceived = new JScrollPane();
+        jScrollPanelReceived.getViewport().add(receivedMsg);
+
+
+        JScrollPane jScrollPanelReply = new JScrollPane();
+        jScrollPanelReply.getViewport().add(replyMsg);
 
         mainPanel.add(rvDeamonText);
         mainPanel.add(rvDeamon);
@@ -109,6 +139,12 @@ public class MainFrame extends JFrame implements ActionListener {
         mainPanel.add(jScrollPane);
         mainPanel.add(jScrollPaneStatus);
         mainPanel.add(sendMsg);
+        mainPanel.add(listenerText);
+        mainPanel.add(listener);
+        mainPanel.add(received);
+        mainPanel.add(jScrollPanelReceived);
+        mainPanel.add(replyCheckBox);
+        mainPanel.add(jScrollPanelReply);
 
         return mainPanel;
     }
@@ -126,10 +162,11 @@ public class MainFrame extends JFrame implements ActionListener {
             isConnected.setText("Connected");
             isConnected.setBackground(Color.GREEN);
             sendMsg.setEnabled(true);
+            listener.setEnabled(true);
             RvCommunication.getInstance().init();
         }
 
-        if(e.getActionCommand().equals("Disconnect")){
+        if (e.getActionCommand().equals("Disconnect")) {
             RvCommunication.getInstance().close();
             rvDeamon.setEditable(true);
             rvNetwork.setEditable(true);
@@ -139,12 +176,39 @@ public class MainFrame extends JFrame implements ActionListener {
             isConnected.setText("Not Connected");
             connect.setText("Connect");
             sendMsg.setEnabled(false);
+            listener.setEnabled(false);
             status.setBackground(this.getBackground());
         }
 
         if (e.getActionCommand().equals("Send")) {
             RvCommunication rvCommunication = RvCommunication.getInstance();
             rvCommunication.sendMsg(rvMsg.getText());
+        }
+
+
+        if (e.getActionCommand().equals("Start Listener")) {
+            new Thread(RvCommunication.getInstance()).start();
+            listenerText.setText("Listening on: " + rvSubject.getText() + " subject");
+            listener.setText("Stop Listener");
+        }
+
+        if (e.getActionCommand().equals("Stop Listener")) {
+            RvCommunication.getInstance().stopListening();
+            listenerText.setText("Not Running");
+            listener.setText("Start Listener");
+        }
+
+    }
+
+    public static void newMsg(RVMessageInterfaceWraper msg) {
+        receivedMsg.append(NEW_MSG);
+        receivedMsg.append(msg.getText());
+        if (replyCheckBox.isSelected()) {
+            try {
+                RvCommunication.getInstance().sendReply(new RVMessageInterfaceWraper(replyMsg.getText()), msg);
+            } catch (TibrvException e) {
+                status.append(e.getMessage());
+            }
         }
     }
 }
--- RvSender/src/rv_wrappers/RvCommunication.java
+++ RvSender/src/rv_wrappers/RvCommunication.java
@@ -11,7 +11,7 @@ import java.awt.*;
 /**
  * Created by JLyc on 9. 4. 2015.
  */
-public class RvCommunication implements TibrvMsgCallback {
+public class RvCommunication implements TibrvMsgCallback, Runnable {
     private static final Log LOG = LogFactory.getLog(RvCommunication.class);
 
     private static RvCommunication rvCommunication;
@@ -25,6 +25,11 @@ public class RvCommunication implements TibrvMsgCallback {
     private TibrvRvdTransport rvdTransport = null;
     private TibrvQueue rvQueue = null;
     private TibrvListener tibrvListener = null;
+
+    public boolean isListening() {
+        return isListening;
+    }
+
     private boolean isListening = true;
 
     public void init() {
@@ -41,6 +46,7 @@ public class RvCommunication implements TibrvMsgCallback {
             MainFrame.getStatus().append("\nRV Listening on subject: " + MainFrame.getRvSubject().getText());
         } catch (TibrvException e) {
             MainFrame.getStatus().append("\nRV failed to initialize: "+ e);
+            MainFrame.getConnect().doClick();
         }
     }
 
@@ -56,6 +62,8 @@ public class RvCommunication implements TibrvMsgCallback {
             Tibrv.close();
         } catch (TibrvException e) {
             MainFrame.getStatus().append("\nCould not close connection! "+ e.getMessage());
+        }finally {
+            stopListening();
         }
     }
 
@@ -68,6 +76,7 @@ public class RvCommunication implements TibrvMsgCallback {
             if(rvdTransport==null) {
                 MainFrame.getStatus().append("No Rvd Transport crated\nMsg not send");
                 MainFrame.getStatus().setBackground(Color.RED);
+                isListening=false;
                 return;
             }
                 rvdTransport.send(new RVMessageInterfaceWraper(xmlMsg));
@@ -77,20 +86,29 @@ public class RvCommunication implements TibrvMsgCallback {
         }
     }
 
-//    public void sendReply(CommunicationMessageInterface responseMsg, CommunicationMessageInterface sourceMsg) {
-//        try {
-//            if (responseMsg instanceof RVMessageInterfaceWraper) {
-//                rvdTransport.sendReply(((RVMessageInterfaceWraper) responseMsg).getMessage(), ((RVMessageInterfaceWraper) sourceMsg).getMessage());
-//                rvdTransport.(((RVMessageInterfaceWraper) responseMsg).getMessage(), ((RVMessageInterfaceWraper) sourceMsg).getMessage());
-//            }
-//        } catch (TibrvException e) {
-//            LOG.error("Failed to send response" + responseMsg.getText(), e);
-//        }
-//    }
+    public void sendReply(RVMessageInterfaceWraper responseMsg, RVMessageInterfaceWraper sourceMsg) {
+        try {
+            if(rvdTransport==null) {
+                MainFrame.getStatus().append("No Rvd Transport crated\nMsg not send");
+                MainFrame.getStatus().setBackground(Color.RED);
+                return;
+            }
+                rvdTransport.sendReply(responseMsg.getMessage(), sourceMsg.getMessage());
+        } catch (TibrvException e) {
+            MainFrame.getStatus().append("Failed to send response" + responseMsg.getText()+ e.getMessage());
+        }
+    }
 
     public void run() {
+        isListening=true;
+        MainFrame.getStatus().append("\nRV listener started");
         while(isListening){
             try {
+                if(rvQueue==null){
+                    MainFrame.getStatus().append("\nRV listener stopped. Listening queue not created");
+                    MainFrame.getListener().doClick();
+                    return;
+                }
                 rvQueue.dispatch();
 //                            timed dispatch should make time to listen ?
 //                            rvQueue.timedDispatch();
@@ -104,5 +122,6 @@ public class RvCommunication implements TibrvMsgCallback {
     @Override
     public void onMsg(TibrvListener tibrvListener, TibrvMsg tibrvMsg) {
         MainFrame.getRvMsg().setText(tibrvMsg.toString());
+        MainFrame.newMsg((RVMessageInterfaceWraper) tibrvMsg);
     }
 }
