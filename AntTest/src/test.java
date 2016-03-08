16cd3b506575f77456ee4c170e6523c358489291
JLyc <andrej.socha@gmail.com>
2016-03-08 08:06:39 +0100

    ant

---
+++ AntTest/src/test.java
@@ -0,0 +1,12 @@
+import org.apache.log4j.Logger;
+
+/**
+ * Created by JLyc on 3/7/2016.
+ */
+public class test {
+    static Logger logger = Logger.getLogger(test.class);
+    public static void main(String[] args) {
+        System.out.println("fuck you");
+        logger.info("fuck you too");
+    }
+}
