8a7f43c70d02042ef2edeb3e836f13fdedbc9f37
JLyc <Jlyc.Development@gmail.com>
2016-04-20 15:45:14 +0200

    xlxs update (fialed aproach no enought dat in xlxs will generate our own)

--- XSLTapi/src/LoadXSLT.java
+++ XSLTapi/src/LoadXSLT.java
@@ -32,35 +32,127 @@ public class LoadXSLT {
         XSSFWorkbook workbook = new XSSFWorkbook(fIP);
         for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
             XSSFSheet spreadsheet = workbook.getSheetAt(i);
+//            System.out.println(spreadsheet.getSheetName());
+            if (!spreadsheet.getSheetName().equals("EAR SAI")) {
+                continue;
+            }
+            StringBuilder sb = new StringBuilder();
             Iterator<Row> rowIterator = spreadsheet.iterator();
             while (rowIterator.hasNext()) {
                 row = (XSSFRow) rowIterator.next();
+                System.out.println(row.getRowNum()+" "+row.getRowStyle());
+                if(row.getRowStyle()!=null){
+                System.out.println(row.getRowNum()+" "+row.getRowStyle().getBorderBottom());}
                 Iterator<org.apache.poi.ss.usermodel.Cell> cellIterator = row.cellIterator();
+                if (row.getRowNum() < 4) {
+                    if (row.getRowNum() == 3)
+                        analyzeTable(row);
+                    continue;
+                }
                 while (cellIterator.hasNext()) {
                     org.apache.poi.ss.usermodel.Cell cell = cellIterator.next();
-                    switch (cell.getCellType()) {
-                        case Cell.CELL_TYPE_NUMERIC:
-                            System.out.print(
-                                    cell.getNumericCellValue() + " \t\t ");
-                            break;
-                        case Cell.CELL_TYPE_STRING:
-                            System.out.print(
-                                    cell.getStringCellValue() + " \t\t ");
-                            break;
+                    if (cell.getColumnIndex() == NAME_COLUMN) {
+                        if(!(sb.lastIndexOf("\n")== sb.length()-1))
+                            sb.append("\n");
                     }
+                    if (cell.getColumnIndex() < VALUE_COLUMN) {
+                        sb.append(readCells(cell));
+                        continue;
+                    }
+                    if (cell.getColumnIndex() == VALUE_COLUMN) {
+                        sb.append(" -> " + readCells(cell));
+                        if (!cellIterator.hasNext()) {
+                            sb.append("\n");
+                        }
+                    }
+                    if (cell.getColumnIndex() == STATUS_COLUMN) {
+                        sb.append(" => " + readCells(cell));
+                        if (!cellIterator.hasNext()) {
+                            sb.append("\n");
+                        }
+                    }
+//                    if (!cellIterator.hasNext()) {
+//                        sb.append("\n");
+//                    }
+
                 }
-            }
 
-
-            if (file.isFile() && file.exists()) {
-                System.out.println(
-                        "openworkbook.xlsx file open successfully.");
-            } else {
-                System.out.println(
-                        "Error to open openworkbook.xlsx file.");
+//                while (cellIterator.hasNext()) {
+//                    Cell cell = cellIterator.next();
+//                    sb.append(readCells(cell));
+//                    if (cellIterator.hasNext()) {
+//
+//                        if (cell.getColumnIndex() == 5 && cell.getCellType() != 3) {
+//                            sb.append("\t->\t");
+//                            sb.append(readCells(cell));
+//                            if (!cellIterator.hasNext()) {
+//                                sb.append("\t=>\tstate\n");
+//                                continue;
+//                            }
+//                            cell = cellIterator.next();
+//                            if (cell.getCellType() != 3) {
+//                                sb.append("\t=>\t");
+//                                sb.append(readCells(cell));
+//                                sb.append("\n");
+//                            } else {
+//                                sb.append("\t=>\tstate\n");
+//                            }
+//                            continue;
+//                        }
+//                        sb.append(readCells(cell));
+//                    } else {
+//                        sb.append("\\");
+//                    }
+//                }
             }
+//                System.out.println();
+            System.out.println(sb);
         }
 
 
+        if (file.isFile() && file.exists()) {
+            System.out.println(
+                    "openworkbook.xlsx file open successfully.");
+        } else {
+            System.out.println(
+                    "Error to open openworkbook.xlsx file.");
+        }
+    }
+
+
+    private static int NAME_COLUMN;
+    private static int VALUE_COLUMN;
+    private static int STATUS_COLUMN;
+
+    private void analyzeTable(XSSFRow row) {
+        Iterator<org.apache.poi.ss.usermodel.Cell> cellIterator = row.cellIterator();
+        while (cellIterator.hasNext()) {
+            org.apache.poi.ss.usermodel.Cell cell = cellIterator.next();
+//            System.out.println(cell.getStringCellValue());
+            switch (readCells(cell)) {
+                case "Name ":
+                    NAME_COLUMN = cell.getColumnIndex();
+                    break;
+                case "Value ":
+                    VALUE_COLUMN = cell.getColumnIndex();
+                    break;
+                case "Modification Status ":
+                    STATUS_COLUMN = cell.getColumnIndex();
+                    break;
+            }
+        }
+    }
+
+    private String readCells(Cell cell) {
+        switch (cell.getCellType()) {
+            case Cell.CELL_TYPE_BOOLEAN:
+                return (cell.getBooleanCellValue() + " ");
+            case Cell.CELL_TYPE_NUMERIC:
+                return (cell.getNumericCellValue() + " ");
+            case Cell.CELL_TYPE_STRING:
+                return (cell.getStringCellValue() + " ");
+            default:
+                return (" ");
+        }
     }
 }
